c133da23620a7da69c694da4c30a3d24b82f688c
Maloth Naresh <idofmalothnaresh@gmail.com>
2018-03-02 14:37:02 +0530

    Forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NbRegisterComponent } from './auth/components/register/register.component';
+import { NbRequestPasswordComponent } from './auth/components/request-password/request-password.component';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 import { NbAuthComponent } from './auth/components/auth.component';
+import { NbRegisterComponent } from './auth/components/register/register.component';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
   { path: 'auth', component: NbAuthComponent },
   { path: 'register', component: NbRegisterComponent },
+  { path: 'request-password', component: NbRequestPasswordComponent },
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth' },
 ];
