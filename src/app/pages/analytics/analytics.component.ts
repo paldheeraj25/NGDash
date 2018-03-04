8fef2141260f778228dd920b61407e178708e99a
Maloth Naresh <idofmalothnaresh@gmail.com>
2018-03-04 20:14:00 +0530

    Charts changes

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -7,68 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
-  view: any[] = [700, 400];
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  data:any = {
+    labels: ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [{
+      data: [57, 65, 59, 80, 81, 56, 55, 40],
+      label: 'Series A'
+    }, {
+      data: [58, 28, 48, 40, 19, 86, 27, 90],
+      label: 'Series B'
+    }],
   };
-
-  single = [
-      {
-        "name": "Germany",
-        "value": 8940000
-      },
-      {
-        "name": "USA",
-        "value": 5000000
-      },
-      {
-        "name": "France",
-        "value": 7200000
-      }
-    ];
-  multi = [
-    {
-      "name": "Germany",
-      "series": [
-        {
-          "name": "2010",
-          "value": 7300000
-        },
-        {
-          "name": "2011",
-          "value": 8940000
-        }
-      ]
-    },
-  
-    {
-      "name": "USA",
-      "series": [
-        {
-          "name": "2010",
-          "value": 7870000
-        },
-        {
-          "name": "2011",
-          "value": 8270000
-        }
-      ]
-    },
-  
-    {
-      "name": "France",
-      "series": [
-        {
-          "name": "2010",
-          "value": 5000002
-        },
-        {
-          "name": "2011",
-          "value": 5800000
-        }
-      ]
-    }
-  ];
 
   constructor() { }
 
