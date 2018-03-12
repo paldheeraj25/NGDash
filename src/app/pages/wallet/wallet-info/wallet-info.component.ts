6ea83a1775fd9effb4f750b4a35dbe6e584f733a
Dheeraj Pal <paldheeraj25@gmail.com>
2018-03-13 00:21:43 +0530

    added module

--- src/app/pages/wallet/wallet-info/wallet-info.component.ts
+++ src/app/pages/wallet/wallet-info/wallet-info.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NbThemeService, NbColorHelper } from '@nebular/theme';
-import { PaymentService } from '../../providers/payment.service';
-import { AuthService } from '../../../auth/providers/auth.service';
-import { find } from 'lodash';
-
 
 @Component({
   selector: 'wallet-info',
@@ -12,25 +8,9 @@ import { find } from 'lodash';
 })
 export class WalletInfoComponent implements OnInit {
 
-  bolltTokens: number = 0;
-  bolttWallet: string;
-  bolttSeed: string;
-  constructor(private walletService: PaymentService, private user: AuthService) { }
-
+  constructor() { }
 
   ngOnInit() {
-    this.getWalletData();
   }
 
-  getWalletData() {
-    const storage = window.localStorage.getItem('bolttUser');
-    const currentUser = this.user.getUser();
-    console.log(currentUser.user_details.user_id_pk);
-    this.walletService.getUserWaveAsset({ user_id: currentUser.user_details.user_id_pk }).subscribe(response => {
-      this.bolltTokens = find(response.data, { name: 'KDP' }).balance;
-      console.log(response)
-      this.bolttWallet = response.user_wallet_data.address;
-      this.bolttSeed = response.user_wallet_data.seed;
-    });
-  }
 }
